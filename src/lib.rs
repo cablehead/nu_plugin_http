646ff458eb9a4d40af471f0faaeaa9ab219cd98c
Andy Gayton <andy@thecablelounge.com>
2024-06-24 13:23:47 -0400

    add version for latest nushell

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,10 @@ mod plugin;
 pub use plugin::HTTPPlugin;
 
 impl Plugin for HTTPPlugin {
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
+
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![
             Box::new(crate::commands::HTTPRequest),
